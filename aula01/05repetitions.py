cda2c6f6de7af94bbde878a6df3156c8f531cc9c
leonardosolar <leonardosolar@yahoo.com.br>
2025-07-25 15:31:03 -0300

    feat: aula 01 basic

--- aula01/05repetitions.py
+++ aula01/05repetitions.py
@@ -1,3 +1,16 @@
+for x in range(9):
+    print(x)
+print("---x---")
+
 for x in range(5):
     print("Repetição número:", x + 1)
 print("Repetições concluídas.")
+
+
+bancodedados = []
+
+for x in range(2):
+    nome = input("Digite seu nome: ")
+    password = int(input("Digite a senha: "))
+    bancodedados.append((nome, password))
+    print("Dados inseridos no banco de dados:", bancodedados)
---
+++ aula01/06dictionaries_objects.py
@@ -0,0 +1,12 @@
+pessoa = {
+    "nome": "leo",
+    "password": 1234,
+    "email": "leo@gmail.com"
+}
+
+print(pessoa)
+print("Dicionário criado:", pessoa)
+print("Nome:", pessoa["nome"])
+print("Senha:", pessoa["password"])
+print("Email:", pessoa["email"])    
+print("Dicionário manipulado com sucesso.")
